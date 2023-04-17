5c314bccb93d1d79ef9ea667ae1872fd4a61fde9
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-17 10:24:13 +0200

    start

---
+++ jour1/01-hello.js
@@ -0,0 +1,26 @@
+console.log("bonjour les amis");
+
+//<script></script> dans un fichier.html
+// exécuter le fichier. html dans un navigateur web => Open With Live Server
+// for node you do it directly in terminal when you create your fille(you put the name)
+
+// cd jour1
+// node 01-hello.js
+// binaire fichier-a-executer.js
+
+// tout ce que l'on a vu en javasvript
+// variable => string /number / boolean /array /objet
+//traitement sur ces types : concaténation/condition/ fonction .map() .reduce()
+// + ++ -- ...
+// if switch for for of ....
+//function anonyme / fonction () => {}
+// objet
+
+// TOUT SAUF window
+//console.log(window); // n'existe pas => déclenche une erreur fatale
+// erreur dans la console  Nodejs
+// at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
+//at node:internal/main/run_main_module:23:47
+
+// la stack d'exécution => pile d'execution => l'enchainement de l'exécution
+// du terminal => fichier 
---
+++ jour1/02-exo.js
@@ -0,0 +1,10 @@
+
+// créer un fichier 02-exo.js => ce fichier contient une variable 
+// un tableau qui s'appelle semaine qui contient les valeurs suivantes 
+
+// ["lundi", "mardi", "mercredi "]
+
+// ajotuer à la fin de la variable le mot "jeudi"
+// pouvez transformer le tableau  ["lundi", "mardi", "mercredi ", "jeudi"]
+// pouvez transformer le tableau  ["LUNDI", "MARDI", "MERCREDI ", "JEUDI"]
+// afficher dans le terminal le tableau transformé
