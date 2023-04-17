880f550b559bab50676be16caa5902cce9b70fa3
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-17 15:46:08 +0200

    node.js

---
+++ jour1/08-download.js
@@ -0,0 +1,25 @@
+//npx..
+//npm install
+
+
+//npmjs.com => package => ensemble de fichiers js
+// npmjs.com=> github spécialisé dans les modules dédiés à nodejs
+
+// si on a besoin de télécharger un module qui est stocké sur npmjs.com
+
+// npm => Node Package Manger
+// binaire est installé en même temps que node
+
+
+//npm --version/-v
+
+//npm init => créer un fichier package.json dans le dossier jour1
+// cls
+// npm i
+//npm install <pkg>
+
+// package.json fr
+
+// lodash=> https://www.npmjs.com/package/lodash
+//
+// npm istall
