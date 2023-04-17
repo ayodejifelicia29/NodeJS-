4d4dafc6c87583c8d0227388efc2faf2d031ff71
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-17 12:16:27 +0200

    créer un fichier avec node

--- jour1/03-exemple.js
+++ jour1/03-exemple.js
@@ -4,7 +4,24 @@
 
 module.exports.a = ["lundi" ,"dimanche"];
 module.exports.phrase = "lorém  ipsum" ;
+/* 
+module.exports = {
+    getId : getId ,
+    a : a ,
+    phrase : phrase
+}
+*/
 
 module.exports.test =() => {
 	 console.log("je suis le test");
 }
+/* module.exports = { 
+    // attention ça écrase les module.exports
+    test : test
+} */
+/* module.exports = {
+    getId : getId ,
+    a : a ,
+    phrase : phrase
+}
+ */
---
+++ jour1/04-module-natif.js
@@ -0,0 +1,25 @@
+// module.exports => exporter
+//const {nom}= require("./nom-fichier.js")// extension du fichier est facultative
+
+const{mulitiplication} =require ("./03-short")
+const fs =require("fs") // fs => module gblobal et natif de nodejs
+                        // ne pas mettre "./fs"
+mulitiplication(10, 20);
+
+//pas besoin de créer un fichier
+//ce sont des fonctions/ traitement qui sont disponibles AVEC NOdeJs
+// modules natifs 
+// 30 modules => qui contiennent 50 fonctions 
+// https://nodejs.org/dist/latest-v18.x/docs/api/fs.html
+
+// https://nodejs.org/en/
+
+// je veux créer un fichier 04-exemple.txt 
+// dans ce fichier je veux écrire le texte "bonjour !!!"
+//
+
+// je veux créer un fichier 
+fs.writeFile("04-exemple.txt" ,"bonjour!!!" ,'utf8' ,function(){
+	console.log("le fichier est bien créé");
+})
+
