202ceb8c7aa4472acc3b57c43e262132458136e9
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-18 10:18:07 +0200

    express

---
+++ jour2-express/index.js
@@ -0,0 +1,8 @@
+// index.js => fichier principal (le pointn d'entrée dev votre application)
+//en html => il faut 0BLIGATOIREMENT que le fichier d'ent
+
+
+
+// dans le monde de nodejs => main.js => main.js/ serveur.js / aap.js
+console.log("serveur");
+
